30208bb10531e1c0e168bc8ca9230e14b6f5f394
Earle-Poole <eepoole4@gmail.com>
2018-10-30 15:50:04 -0500

    Updates

---
+++ precache-manifest.39014c8d580b8c111c8357cea5491975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "002c4e9bd45a266a78e3",
+    "url": "/facerecognitionbrain/static/js/runtime~main.002c4e9b.js"
+  },
+  {
+    "revision": "9d9a9a04fbfeab85a715",
+    "url": "/facerecognitionbrain/static/js/main.9d9a9a04.chunk.js"
+  },
+  {
+    "revision": "bd56f38be8d3a0768caa",
+    "url": "/facerecognitionbrain/static/js/1.bd56f38b.chunk.js"
+  },
+  {
+    "revision": "9d9a9a04fbfeab85a715",
+    "url": "/facerecognitionbrain/static/css/main.4fc20fc4.chunk.css"
+  },
+  {
+    "revision": "bd56f38be8d3a0768caa",
+    "url": "/facerecognitionbrain/static/css/1.02fc45ba.chunk.css"
+  },
+  {
+    "revision": "e99cc12a963da490fb319c335d3f8c29",
+    "url": "/facerecognitionbrain/index.html"
+  }
+];
